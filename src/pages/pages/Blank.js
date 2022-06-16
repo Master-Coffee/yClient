701a14bc2fc41ac30a7eed773f84fa50299552bd
Master-Coffee <98613910+Master-Coffee@users.noreply.github.com>
2022-06-16 20:41:44 -0300

    Commit 2
    Change on the Input form style

--- src/pages/pages/Blank.js
+++ src/pages/pages/Blank.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 import * as XLSX from "xlsx";
 import axios from 'axios';
+
+
+
 
 const readExcel = (file) => {
   const promise = new Promise((resolve, reject) => {
@@ -13,31 +16,24 @@ const readExcel = (file) => {
     fileReader.onload = (e) => {
       const bufferArray = e.target.result;
       const wb = XLSX.read(bufferArray, { type: "buffer" });
-      
       const wsname = wb.SheetNames;              // lista com o nome das folhas
       const ws_list = wsname.map((ws) => {       // lista com os conteúdos das folhas 
         const data = wb.Sheets[ws];              // Conteúdo de uma folha
         return XLSX.utils.sheet_to_json(data);; 
       })
-
       resolve([ws_list, wsname])
     };
-    
     fileReader.onerror = (error) => {reject(error);};
-
   });
-  
   promise.then((params) => {
-    
     var ws_list = params[0]
     var wsname = params[1]
-    
     ws_list.map((lista_insumo, index) => ( //Inserindo o nome da folha nos insumos 
       lista_insumo.map( insumo => (
         insumo.sheet  = wsname[index]
       ))
     ))
-  
+
     var auxArray = [];
     for (var j = 0; j <= ws_list.length ; j++ ) { auxArray = auxArray.concat(ws_list[j])};
   
@@ -75,6 +71,7 @@ const readExcel = (file) => {
     })
     .then(response => {console.log(response.status)})
 
+
     /*  
     fetch('http://localhost:5000/insumo',{
       method: 'Post',
@@ -85,35 +82,49 @@ const readExcel = (file) => {
       body:JSON.stringify(payload)
     }).then(response => {console.log(response.status)})
     */
+
   }) 
 }
+
+const InputForm = () => (
+  <Card>
+    <Card.Header>
+      <Card.Title>Import Supermicro Data</Card.Title>
+    </Card.Header>
+    <Card.Body>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Excel input</Form.Label>
+          <Form.Control type="file" name="file" onChange={ e => { 
+            const file = e.target.files[0];
+            readExcel(file);
+          }}/>
+          <Form.Text className="text-muted">
+            The file must be in .xlsx format.
+          </Form.Text>
+        </Form.Group>
+        <Button variant="primary">Submit</Button>
+
+      </Form>
+    </Card.Body>
+  </Card>
+);
+
 
 
 const Blank = () => (
   <React.Fragment>
-    <Helmet title="Blank Page" />
+    <Helmet title="Form Layouts" />
     <Container fluid className="p-0">
-      <h1 className="h3 mb-3">Blank Page</h1>
+      <h1 className="h3 mb-3">Form Layouts</h1>
 
       <Row>
-        <Col>
-          <Card>
-            <Card.Header>
-              <Card.Title tag="h5" className="mb-0">
-                
-                <div> 
-                  <input  type = "file"  onChange = {(e) => {
-                    const file = e.target.files[0];
-                    readExcel(file);
-                  }} /> 
-                </div>
-                
-              </Card.Title>
-            </Card.Header>
-            <Card.Body>&nbsp;</Card.Body>
-          </Card>
+        <Col lg="6"></Col> 
+        <Col lg="6">
+          <InputForm/>
         </Col>
       </Row>
+
     </Container>
   </React.Fragment>
 );
